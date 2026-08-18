dc726ee146d556903734cf70dcf00ba0e8fb889e
NTran <NTran@DESKTOP-TU68I8R>
2026-08-18 17:30:42 +0700

    Add studeAdd student manager projects.nt manager projects

--- StudentManagerV2MultipleConstructors/Program.cs
+++ StudentManagerV2MultipleConstructors/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace StudentManagerV2MultipleConstructors
+﻿using StudentManagerV2MultipleConstructors.Entities;
+
+namespace StudentManagerV2MultipleConstructors
 {
-    internal class Program
+    public class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int xxx;
+            //Console.WriteLine(xxx);
+            // biến khai bao kgan value --> loi NGAY LUC GO VI LAY RAC TRONG RAM 
+
+            var s3 = new Student("SE3", "David"); 
+            Console.WriteLine("se3 detail with default values inside ");
+            Console.WriteLine(s3); // toString 
+
+            // cobnh constructor co bay nhieu cach new 
+            // gia tri default neu ko dien du info - ko mang rac nhu bien local 
+
+            s3.SetYob(1987);
+            s3.SetGpa(10);
+            Console.WriteLine(" student's details after setting");
+            Console.WriteLine(s3);
+            // ham GET /SET GOI THOAI MAI , N LAN TREN TUNG OBJECT MA HAM KHONG TAO VUNG NEW MOI
+            // NO CHI CHINH SUA INFO CUA OBJ , VUNG NEW DA TUNG TAO TRUOC DO 
+            Student s4 = new Student("SE4", "Eli", 2002, 3.4);
+
         }
     }
 }
