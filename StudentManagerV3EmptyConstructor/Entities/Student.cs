cdb2d185d0d37e563e37df86fbc5c0a2184078b2
localadmin <karperth3112@gmail.com>
2026-08-24 09:52:53 -0700

    update student manager logic

--- StudentManagerV3EmptyConstructor/Entities/Student.cs
+++ StudentManagerV3EmptyConstructor/Entities/Student.cs
@@ -4,5 +4,47 @@ namespace StudentManagerV3EmptyConstructor.Entities;
 
 public class Student
 {
+    private string _id;
+    private string _name;
+    private double _gpa;
+    private int _yob;
     
+    public string GetName()
+    {
+        return _name; 
+    }
+    public string GetId()
+    {
+        return _id;
+    }
+    public double  GetGpa()
+        {
+        return _gpa;
+        }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public void SetGpa(double gpa)
+    {
+        _gpa = gpa;
+    }
+    public void SetYob(int yob)
+    {
+        _yob = yob; 
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {_name}, Gpa: {_gpa}, Yob: {_yob}"; 
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine("Student's Information");
+        Console.WriteLine($"Name: {_name}, Gpa: {_gpa}, Yob: {_yob}");
+        
+    }
 }
--- StudentManagerV3EmptyConstructor/Program.cs
+++ StudentManagerV3EmptyConstructor/Program.cs
@@ -6,7 +6,26 @@ class Program
 {
     static void Main(string[] args)
     {
+        // nếu  class không có constructor 
+        // ta vẫn luôn khuôn ra đúc, đúc được 1 object empty ~ object chứa ko khí bên trong 
+        // giống đi photo 1 cái form , chưa điền gì cả
+        // java,c# provide cho bạn1 cái constructor empty , ko đầu vào giúp mô tả hành động đúc 1 object mang giá trị default 
         
+        //object trống trơn , giống tờ giấy form trống
+        //điền từ từ sau , tương ứng hàm Set()
+        //Set() đến đâu ta xem đến đó, hàm Get()
+        //obj luôn được tạo ra
+        
+        Student s1 = new Student(); //constructor tự sinh ra 
+        Student s2 = new Student();
+        Student s3 = new Student();
+        
+    // set . get, toString , showprofile 
+        Console.WriteLine(s1.ToString());
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+        
+
 
     }
 }
